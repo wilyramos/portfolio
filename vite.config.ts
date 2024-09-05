b33332fff203b2777a659a3492bdc8e59bff1e04
wily ramos <wily.ramos1@unmsm.edu.pe>
2024-09-04 19:11:44 -0500

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://wilyramos/portfolio/',
+  base: 'https://wilyramos.github.io/portfolio/',
 })
